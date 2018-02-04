bf3cd6b3a77ff8e760b6fddb6d39ff33d9b06816
Brad Dougherty <me@brad.is>
2018-02-03 23:35:19 -0500

    Add config options

--- src/carbon.ts
+++ src/carbon.ts
@@ -1,7 +1,32 @@
 'use strict';
 
-import { ExtensionContext, commands, window, TextDocument, Uri } from 'vscode';
+import { ExtensionContext, commands, window, TextDocument, Uri, workspace } from 'vscode';
 import { open } from 'openurl';
+import { URL } from 'url';
+
+function getLanguage(languageId: string) {
+    const languageMap = new Map([
+        ['c', 'text/x-csrc'],
+        ['csharp', 'text/x-csharp'],
+        ['cpp', 'text/x-c++src'],
+        ['diff', 'text/x-diff'],
+        ['fsharp', 'mllike'],
+        ['html', 'htmlmixed'],
+        ['java', 'text/x-java'],
+        ['javascriptreact', 'jsx'],
+        ['json', 'application/json'],
+        ['objective-c', 'text/x-objectivec'],
+        ['php', 'text/x-php'],
+        ['plaintext', 'text'],
+        ['shellscript', 'application/x-sh']
+    ]);
+
+    if (languageMap.has(languageId)) {
+        return languageMap.get(languageId);
+    }
+
+    return languageId;
+}
 
 export function activate(context: ExtensionContext) {
     const disposable = commands.registerCommand('carbon.show', () => {
@@ -9,10 +34,23 @@ export function activate(context: ExtensionContext) {
 
         if (!editor) return window.showErrorMessage('😱 Feed me code!');
 
-        const { getText, languageId } = editor.document;
+        const settings = workspace.getConfiguration('carbon');
+        const language = getLanguage(editor.document.languageId);
 
-        const url = `https://carbon.now.sh/?bg=rgba(0,0,0,0)&t=dracula&l=${languageId}&ds=true&wc=true&wa=true&pv=43px&ph=57px&ln=false&code=${encodeURI(getText())}`;
-        
+        const url = new URL(settings.get('url'));
+        url.searchParams.set('bg', settings.get('backgroundColor'));
+        url.searchParams.set('t', settings.get('theme'));
+        url.searchParams.set('l', language);
+        url.searchParams.set('ds', settings.get('dropShadow'));
+        url.searchParams.set('wc', settings.get('windowControls'));
+        url.searchParams.set('wa', settings.get('autoAdjustWidth'));
+        url.searchParams.set('pv', `${settings.get('paddingVertical')}px`);
+        url.searchParams.set('ph', `${settings.get('paddingHorizontal')}px`);
+        url.searchParams.set('ln', settings.get('lineNumbers'));
+        url.searchParams.set('f', settings.get('fontFamily'));
+        url.searchParams.set('fs', `${settings.get('fontSize')}px`);
+        url.searchParams.set('code', editor.document.getText());
+
         open(url);
     });
 
